0fc72246582b0b6545c32dfc54fa4ebfbc86e982
jincepgeorge <cag.kzk.media2@gmail.com>
2026-01-13 16:37:19 +0530

    firebase added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // TODO: Replace with your actual Firebase config from Firebase Console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAuRvDyTlGQY7pF0tZSjNL40ht7d94lBB0",
+  authDomain: "bga-cs-talent-test-app.firebaseapp.com",
+  projectId: "bga-cs-talent-test-app",
+  storageBucket: "bga-cs-talent-test-app.firebasestorage.app",
+  messagingSenderId: "588423733482",
+  appId: "1:588423733482:web:c62cd3acf645875f77f793",
+  measurementId: "G-2X6ZPX2Q5N"
 };
 
 // Initialize Firebase
